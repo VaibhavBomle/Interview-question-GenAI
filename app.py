18771a5d4869e7206737e077ef671b547014638d
vaibhav <bomlevaihav@gmail.com>
2024-10-21 16:54:06 +0530

    implemented API to upload a file using fastapi

---
+++ app.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI,Form,Request,Response,File,Depends,HTTPException,status
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.encoders import jsonable_encoder
+import uvicorn
+import os
+import aiofiles
+import json
+import csv
+from src.helper import llm_pipeline
+
+app = FastAPI()
+
+app.mount("/static",StaticFiles(directory="static"),name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+async def index(request:Request):
+    return templates.TemplateResponse("index.html",{"request":request})
+
+@app.post("/upload")
+async def chat(request:Request,pdf_file:bytes = File(), filename:str = Form(...)):
+    base_folder = 'static/docs/'
+    if not os.path.isdir(base_folder):
+        os.mkdir(base_folder)
+    pdf_filename = os.path.join(base_folder,filename)
+
+    async with aiofiles.open(pdf_filename,filename) as f:
+        await f.write(pdf_file)
+
+    response_data = jsonable_encoder(json.dumps({"msg": 'success',"pdf_filename": pdf_filename}))
+    res = Response(response_data)
+    return res
+
+def get_csv(file_path):
+    answer_generation_chain, ques_list = llm_pipeline(file_path)
